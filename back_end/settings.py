8a05052340a63d513bdcd258af6e7cb4b728632b
joe <joe@krolm.com>
2020-03-09 18:42:32 +0800

    Settings

--- back_end/settings.py
+++ back_end/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.path.join(BASE_DIR, 'db.sqlite3')),
+    'default': dj_database_url.config(default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
 }
 
 # Password validation
