4a882c5063b0f70a4aaab7c31b12e3d97a723555
joe <joe@krolm.com>
2020-03-09 18:10:44 +0800

    DB Setting

--- back_end/settings.py
+++ back_end/settings.py
@@ -84,9 +84,10 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}.'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
